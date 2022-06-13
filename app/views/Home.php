53c9b9e0ce65ebbe35b85ce6116ec7c147b009ad
LG64BIT <leon.gal@hotmail.com>
2022-06-13 15:11:49 +0200

    Added registration feature

--- app/views/Home.php
+++ app/views/Home.php
@@ -1,1 +1,10 @@
 <?php
+namespace app\views;
+include "Header.php";
+?>
+<a class="btn btn-primary" href="login">Login</a>
+<a class="btn btn-primary" href="register">Register</a>
+<h1>Web Shop</h1>
+<?php
+include "Footer.php";
+?>
